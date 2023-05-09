c912de335b0afbfd8b03653d3275d8a870fd2746
Sergey Piunov <piunov92@gmail.com>
2023-05-09 20:50:38 +0300

    updated class App, AppConfig, AppTest and created new class Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = applicationContext.getBean(HelloWorld.class);
         System.out.println(bean.getMessage());
+        System.out.println(bean2.getMessage());
+        System.out.println(bean == bean2);
+
+        System.out.println();
+
+        Cat beanCat = applicationContext.getBean(Cat.class);
+        Cat beanCat2 = applicationContext.getBean(Cat.class);
+        System.out.println(beanCat.getName());
+        System.out.println(beanCat2.getName());
+        System.out.println(beanCat == beanCat2);
     }
 }
